165e0b8a16d95159e63d3774c05fa7a04a5dce6a
Sara Cirone <114257693+SaraCirone@users.noreply.github.com>
2022-10-25 17:08:37 +0200

    Add files via upload

---
+++ W4-d2/assets/js/script.js
@@ -0,0 +1,29 @@
+btn = document.getElementById('spendi'); //definisce il pulsante btn come quello collegato ad ID Spendi
+
+btn.addEventListener('click', () => {  
+    
+    document.getElementById('totale').innerHTML = '';  //al click di spendi cancella sempre in automatico tutto il contenitore
+    var budget = 1000; //ridefinisce ogni volta il budget a 1000 ad ogni click
+    
+    while (budget > 200) {
+        let spent = Math.floor(Math.random() * 100);//genera il numero random
+
+        if (spent > budget) { //fa il confronto con il budget
+            spent = budget; //ridefinisce spesa
+        }
+
+        budget -= spent; //fa la sottrazione tra budget e spesa. 
+        
+        document.getElementById('totale').innerHTML += `<p>Hai speso <b>${spent}</b>€`; //scrive quanto hai speso.
+
+        if (budget > 200) {
+            document.getElementById('totale').innerHTML += '<p><i>Hai ancora soldi da spendere</i></p>';
+        } else if (budget < 200) {
+            document.getElementById('totale').innerHTML += '<p><i>Hai superato il tuo budget</i></p>';
+
+        }
+
+
+        document.getElementById('totale').innerHTML += `<p>Ti rimangono <b>${budget}€</b></p>`;
+    
+});
