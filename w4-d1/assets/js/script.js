887de68ea5c4b6292cb8acbc67280367fdf6cb03
Sara Cirone <114257693+SaraCirone@users.noreply.github.com>
2022-12-05 15:55:35 +0100

    Update script.js

--- w4-d1/assets/js/script.js
+++ w4-d1/assets/js/script.js
@@ -1,20 +1,51 @@
-var oggi = new Date(); 
-var m = oggi.getMonth();
-var hours = oggi.getHours();
-var minutes = oggi.getMinutes();
-var second = oggi.getSeconds(); 
+//setta le variabili del New Date necessario e poi la vriabile month e date per poter fare un confronto con la data attuale e poter pescare nei relativi array, il giorno ed il mese (a lettere) corrispondente. 
+let data = new Date(); 
+let month = data.getMonth();
+let day = data.getDay();
 
-const mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+const arrayMesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
 
-document.getElementById('dataOggi').innerHTML = `${oggi.getDate()} - ${mesi[m]} - ${oggi.getFullYear()}`;
+const arrayGiorni = ['Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato'];
 
-//uno
+//stampa la data concatenando numero, mese (in lettere) e anno
+document.getElementById('dataOggi').innerHTML += ` ${data.getDate()}/${arrayMesi[month]}/${data.getFullYear()}`;
 
-document.getElementById("europe").innerHTML = oggi; //stampa formato europeo
-document.getElementById('gg').innerHTML += oggi.getDate();//stampa formato giorno 
-document.getElementById('mese').innerHTML += mesi[m]; //stampa mese nome
-document.getElementById('dataNow').innerHTML = `${oggi.getDate()} - ${oggi.getMonth() + 1} - ${oggi.getFullYear()}`; //data formattata con mese numerico
+//stampa la data completa in formato fuso orario 
+document.getElementById('fullData').innerHTML = data;
+//stampa il numero del giorno
+document.getElementById('giorno'). innerHTML += `${data.getDate()}`;
+//stampa il giorno in lettere
+document.getElementById('giorno2'). innerHTML += `${arrayGiorni[day]}`;
+//stampa il numero del mese
+document.getElementById('mese'). innerHTML += `${data.getMonth()}`;
+//stampa il mese in lettere
+document.getElementById('mese2'). innerHTML += `${arrayMesi[month]}`;
+//stampa la data a numeri concatenando giorno, mese ed anno
+document.getElementById('dataFull').innerHTML += ` ${data.getDate()} / ${data.getMonth()} / ${data.getFullYear()}`
 
+//con un setInterval puntato sulla funzione (ora attuale), si setta un refresh a 1000 millisecondi (1 secondo) per far si che l'orario scorra ogni secondo autocamenticamente.  Dentro (come variabile locale) si setta nuovamente il new date e si stampano poi ora, minuti e secondi. Alla fine si chiama la funzione esternamente. 
+setInterval(oraAttuale, 1000);
+function oraAttuale() {
+    const oraAdesso = new Date; 
+    document.getElementById('orario').innerHTML = `<b>Sono le ore</b> ${oraAdesso.getHours()} : ${oraAdesso.getMinutes()} : ${oraAdesso.getSeconds()}`;
+}
+oraAttuale();
 
-//stampa orario
-document.getElementById('ore').innerHTML = `Sono le ore ${oggi.getHours()}, ${oggi.getMinutes()} minuti, e ${oggi.getSeconds()} secondi`;
+//si setta come prima un setinterval ad (1 secondo) puntato sulla funzione saluto. Si crea un IF che controlla diversi orari ed in base ai parametri inseriti restituisce un saluto
+setInterval(saluto, 1000);
+function saluto() {
+    const ora = new Date; 
+    console.log(ora.getHours());
+
+    if(ora.getHours() >= 6 && ora.getHours() <= 12) {
+        document.getElementById('saluto').innerHTML = `<b>Buongiorno</b>`; 
+    } else if(ora.getHours() >= 13 && ora.getHours() <= 19){
+        document.getElementById('saluto').innerHTML = '<b>Buon Pomeriggio!</b>';  
+    } else if(ora.getHours() >= 20 && ora.getHours() <= 23){
+        document.getElementById('saluto').innerHTML = '<b>Buona Sera!</b>'; 
+    } else {
+        document.getElementById('saluto').innerHTML = '<b>Vai a letto!</b>'; 
+    }
+
+}
+saluto();
